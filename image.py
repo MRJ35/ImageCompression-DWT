e983f48f1629658ee0cf84b69985ed48afcd50d7
NamitS27 <namitshah2711@gmail.com>
2019-12-08 09:15:33 +0530

    New Commit

--- image.py
+++ image.py
@@ -1,13 +1,12 @@
 from PIL import Image, ImageDraw, ImageFont
-
+import os
+import math
 
 def show_output(filename1,filename2):
     im1 = Image.open(filename1)
     im2 = Image.open(filename2)
     a,b = im1.size
-    print(a,b)
-    # im1 = im1.resize((a//2,b))
-    # im2 = im2.resize((a//2,b))
+    
     newI = Image.new('RGBA',(2*a+5,100+b+100),'white')
     newI.paste(im1,(0,100,a,b+100))
     newI.paste(im2,(a+5,100,2*a+5,b+100))
@@ -15,10 +14,11 @@ def show_output(filename1,filename2):
 
     draw = ImageDraw.Draw(newI)
     font = ImageFont.truetype('Lato-Black.ttf', 75) # write the path of the font which you want to select
-    draw.text((a//2 - 150, b+102), "226 Kb", (0, 0, 0), font=font)
-    draw.text(((a+a//2) - 150, b+102), "4.5 Mb", (0, 0, 0), font=font)
-    draw.text((a//2+150,5),"Image Compression",(0,0,0),font=font)
+    draw.text((a//2 - 150, b+102), meta_data(filename1), (0, 0, 0), font=font)
+    draw.text(((a+a//2) - 150, b+102), meta_data(filename2), (0, 0, 0), font=font)
+    draw.text((a//2+ a//4 + a//7,5),"Image Compression",(0,0,0),font=font)
     #newI.show()
     newI.save("abc.png")
 
-#show_output("MERGED.png","NEE.png")
+def meta_data(filen):
+    return str(math.ceil(int(os.path.getsize(filen))/1024)) + " Kb"
