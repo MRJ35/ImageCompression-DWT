719da233e670d27610bfd5ee4186c6d037b3bfed
NamitS27 <namitshah2711@gmail.com>
2019-12-09 23:48:26 +0530

    Fianl Commit

--- get_coeff.py
+++ get_coeff.py
@@ -6,83 +6,83 @@ from PIL import Image
 import utils as util
 
 
-def rgb_to_yuv(img):
-    """
-    Returns yuv image
-    Parameters
-    ----------
-    img: PIL Image
-    Returns
-    -------
-    PIL Image:
-        image converted to yuv
-    """
-    yuv_img = img.convert('RGB')
-    (width, height) = img.size
-    for x in range(width):
-        for y in range(height):
-            (r, g, b) = yuv_img.getpixel((x, y))
-        Y = 0.299 * r + 0.587 * g + 0.114 * b
-        CB = - 0.168935 * r + - 0.331665 * g + 0.50059 * b + 128
-        CR = 0.499813 * r - 0.4187 * g + - 0.081282 * b + 128
-        Y = 255 if (Y >= 255) else Y
-        Y = 0 if (Y <= 0) else Y
-        CB = 255 if (CB >= 255) else CB
-        CB = 0 if (CB <= 0) else CB
-        CR = 255 if (CR >= 255) else CR
-        CR = 0 if (CR <= 0) else CR
-        yuv_img.putpixel((x, y), (int(Y), int(CB), int(CR)))
-    return yuv_img
+# def rgb_to_yuv(img):
+#     """
+#     Returns yuv image
+#     Parameters
+#     ----------
+#     img: PIL Image
+#     Returns
+#     -------
+#     PIL Image:
+#         image converted to yuv
+#     """
+#     yuv_img = img.convert('RGB')
+#     (width, height) = img.size
+#     for x in range(width):
+#         for y in range(height):
+#             (r, g, b) = yuv_img.getpixel((x, y))
+#         Y = 0.299 * r + 0.587 * g + 0.114 * b
+#         CB = - 0.168935 * r + - 0.331665 * g + 0.50059 * b + 128
+#         CR = 0.499813 * r - 0.4187 * g + - 0.081282 * b + 128
+#         Y = 255 if (Y >= 255) else Y
+#         Y = 0 if (Y <= 0) else Y
+#         CB = 255 if (CB >= 255) else CB
+#         CB = 0 if (CB <= 0) else CB
+#         CR = 255 if (CR >= 255) else CR
+#         CR = 0 if (CR <= 0) else CR
+#         yuv_img.putpixel((x, y), (int(Y), int(CB), int(CR)))
+#     return yuv_img
 
 
-def yuv_to_rgb(img):
-    """
-    Returns rgb image
-    Parameters
-    ----------
-    img: PIL Image
-    Returns
-    -------
-    PIL Image:
-        image converted to rgb
-    """
-    (width, height) = img.size
-    rgb_img = img.copy()
-    for x in range(width):
-        for y in range(height):
-            (Y, CB, CR) = img.getpixel((x, y))
-        R = Y + 1.402 * (CR - 128)
-        G = Y - 0.34414 * (CB - 128) - 0.71414 * (CR - 128)
-        B = Y + 1.772 * (CB - 128)
-        R = 255 if (R >= 255) else R
-        R = 0 if (R <= 0) else R
-        G = 255 if (G >= 255) else G
-        G = 0 if (G <= 0) else G
-        B = 255 if (B >= 255) else B
-        B = 0 if (B <= 0) else B
-        rgb_img.putpixel((x, y), (int(R), int(G), int(B)))
-    return rgb_img
+# def yuv_to_rgb(img):
+#     """
+#     Returns rgb image
+#     Parameters
+#     ----------
+#     img: PIL Image
+#     Returns
+#     -------
+#     PIL Image:
+#         image converted to rgb
+#     """
+#     (width, height) = img.size
+#     rgb_img = img.copy()
+#     for x in range(width):
+#         for y in range(height):
+#             (Y, CB, CR) = img.getpixel((x, y))
+#         R = Y + 1.402 * (CR - 128)
+#         G = Y - 0.34414 * (CB - 128) - 0.71414 * (CR - 128)
+#         B = Y + 1.772 * (CB - 128)
+#         R = 255 if (R >= 255) else R
+#         R = 0 if (R <= 0) else R
+#         G = 255 if (G >= 255) else G
+#         G = 0 if (G <= 0) else G
+#         B = 255 if (B >= 255) else B
+#         B = 0 if (B <= 0) else B
+#         rgb_img.putpixel((x, y), (int(R), int(G), int(B)))
+#     return rgb_img
 
 
-def rgb_to_grayscale(img):
-    """
-    Returns greyscale image
-    Parameters
-    ----------
-    img: PIL Image
-    Returns
-    -------
-    PIL Image:
-        image converted to greyscale
-    """
-    width, height = img.size
-    res = img.copy()
-    for i in range(width):
-        for j in range(height):
-            (r, g, b) = img.getpixel((i, j))
-            gs = int((r + g + b) / 3)
-            res.putpixel((i, j), (gs, gs, gs))
-    return res
+# def rgb_to_grayscale(img):
+#     """
+#     Returns greyscale image
+#     Parameters
+#     ----------
+#     img: PIL Image
+#     Returns
+#     -------
+#     PIL Image:
+#         image converted to greyscale
+#     """
+#     width, height = img.size
+#     res = img.copy()
+#     for i in range(width):
+#         for j in range(height):
+#             (r, g, b) = img.getpixel((i, j))
+#             gs = int((r + g + b) / 3)
+#             res.putpixel((i, j), (gs, gs, gs))
+#     return res
 
 
 def extract_rgb_coeff(img):
@@ -117,11 +117,11 @@ def extract_rgb_coeff(img):
     # print(mat_g)
     # print("\nMat_b : ")
     # print(mat_b)
-    coeffs_r = pywt.dwt2(mat_r, 'db1')
+    coeffs_r = pywt.dwt2(mat_r, 'haar')
     # coeffs_g: cA,(cH,cV,cD)
-    coeffs_g = pywt.dwt2(mat_g, 'db1')
+    coeffs_g = pywt.dwt2(mat_g, 'haar')
     # coeffs_b: cA,(cH,cV,cD)
-    coeffs_b = pywt.dwt2(mat_b, 'db1')
+    coeffs_b = pywt.dwt2(mat_b, 'haar')
     # print("Coeff : \ncoeff_r ")
     # print(coeffs_r,"\ncoeff_g ",coeffs_g,"\ncoeff_b ",coeffs_b)
     return (coeffs_r, coeffs_g, coeffs_b)
@@ -181,6 +181,10 @@ def img_from_dwt_coeff(coeff_dwt):
     # Image object init
     dwt_img = Image.new('RGB', (width, height), (0, 0, 20))
     # cA reconstruction
+
+    '''
+    The image formed from the low frequnecy of the images which contains the main content of the image
+    '''
     for i in range(width):
         for j in range(height):
             R = cARed[i][j]
