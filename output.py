33b60032a3d8eb3042ed5cfc291a402386344e5a
NamitS27 <namitshah2711@gmail.com>
2019-12-11 23:02:17 +0530

    new commit

--- output.py
+++ output.py
@@ -12,11 +12,17 @@ def vspace(row,root):
     space = Label(root,text = "  ",bg = '#3ed8ea')
     space.grid(row=row)
 
-def rightClick(event,x):
+def leftClick(event,x):
+    '''
+    Left Click event to open image
+    '''
     im = Image.open(x)
     im.show()
 
 def run(fileList):
+    '''
+    Creates the window 
+    '''
     root = Tk()
     root.title("Image Compression")
     root.geometry('1070x400')
@@ -38,6 +44,9 @@ def run(fileList):
     cr.grid(row=2,column=6)
 
     rows = 4
+    '''
+    Adding the files in the list along with the size of original image and compressed image and also compression ratio
+    '''
     for i in range(len(fileList)):
         name = ntpath.basename(fileList[i])
         compressed_file_name =  "Compressed_Images/"+name[:len(name)-4]+"_compressed"+name[len(name)-4:]
@@ -46,21 +55,32 @@ def run(fileList):
     mainloop()
 
 def extract_ratio(str1,str2):
+    '''
+    Finds the compression ratio
+    '''
     s1 = int(str1[:len(str1)-3])
     s2 = int(str2[:len(str2)-3])
     return "%.2f"%(s1/s2)
     
 def create_table(root,name,file,file2,row):
+    '''
+    This is used for adding multiple images in the list. 
+    On clicking the original image, it opens the original image
+    On clicking the compressed image, it opens the compressed image
+    '''
     image = Label(root,text=name,bg = '#3ed8ea',fg='#1c1c6c',font='Verdana 12')
     image.grid(row=row,column = 0)
     image1 = Label(root,text = meta_data(file),bg = '#3ed8ea',fg='#1c1c6c',font='Verdana 12')
     image1.grid(row=row,column = 2)
-    image1.bind("<Button-1>",lambda event:rightClick(event,file))
+    image1.bind("<Button-1>",lambda event:leftClick(event,file))
     image1 = Label(root,text = meta_data(file2),bg = '#3ed8ea',fg='#1c1c6c',font='Verdana 12')
     image1.grid(row=row,column = 4)
-    image1.bind("<Button-1>",lambda event:rightClick(event,file2))
+    image1.bind("<Button-1>",lambda event:leftClick(event,file2))
     image1 = Label(root,text = extract_ratio(meta_data(file),meta_data(file2)),bg = '#3ed8ea',fg='#1c1c6c',font='Verdana 12')
     image1.grid(row=row,column = 6)
 
 def meta_data(filen):
+    '''
+    Gives the file size of the selected file in Kb
+    '''
     return str(math.ceil(int(os.path.getsize(filen))/1024)) + " Kb"
